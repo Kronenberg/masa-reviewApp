a603a7b663db8343e03c5ccc4c6324bcb9976995
Irena Neidlin <XsolarXwhisperX@gmail.com>
2018-01-03 05:08:45 +0200

    added sass

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App__header">
+          <img src={logo} className="App__logo" alt="logo" />
+          <h1 className="App__title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }

cc48af85753df5553372f5eb26ebf0fffc6b0e9b
Irena Neidlin <XsolarXwhisperX@gmail.com>
2018-01-03 17:17:47 +0200

    add idea-files to .gitignore

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App__header">
-          <img src={logo} className="App__logo" alt="logo" />
-          <h1 className="App__title">Welcome to React</h1>
-        </header>
+    render() {
+        return (
+            <div className="App">
+                <header className="App__header">
+                    <img src={logo} className="App__logo" alt="logo"/>
 
-      </div>
-    );
-  }
+                    <h1 className="App__title">Welcome to React</h1>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;

0c0b94f18c24c768dcce231a22b858ec172a036e
Vladislav Spivakov <spone379@gmail.com>
2018-01-08 01:08:50 +0200

    Move router logic from App.js to routs.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+
+import { NavLink } from 'react-router-dom';
+
+import Routs from './routs';
+
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/program' component={Program} />
-          <Route path='/chat' component={Chat} />
-        </Switch>
+        <nav>
+          <ul>
+            
+            <li><NavLink to="/" activeClassName="selected">Home</NavLink></li>
+            <li><NavLink to="/programs" activeClassName="selected">Programs</NavLink></li>
+            <li><NavLink to="/chat" activeClassName="selected">Chat</NavLink></li>
+            
+          </ul>
+        </nav>
+        <Routs />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render((
 	<BrowserRouter>

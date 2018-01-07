469b855ac5115541649ee06e51833dcd181cd9a1
Vladislav Spivakov <spone379@gmail.com>
2018-01-08 01:07:25 +0200

    Add boilerplates of components

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>Someday I'll be the chat!</div>
+    );
+  }
+}
+
+export default Chat;
---
+++ src/components/Programs/Programs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Programs extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>I contain all programs!</div>
+    );
+  }
+}
+
+export default Programs;

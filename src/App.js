43510536ce4a5863023b7508ba3f0c505c368a52
Vladyslav Spivakov <spone379@gmail.com>
2018-01-08 23:35:04 +0200

    Move router logic in separate file (#18)
    * Add React-router in project
    
    * Add boilerplates of components
    
    * Move router logic from App.js to routs.js
    
    * Resolve conflict in Chat.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
+
+
+import { NavLink } from 'react-router-dom';
+
+import Routs from './routs';
+
+
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 
 
 class App extends Component {
   render() {
     return (
       <div>
+
+        <nav>
+          <ul>
+            
+            <li><NavLink to="/" activeClassName="selected">Home</NavLink></li>
+            <li><NavLink to="/programs" activeClassName="selected">Programs</NavLink></li>
+            <li><NavLink to="/chat" activeClassName="selected">Chat</NavLink></li>
+            
+          </ul>
+        </nav>
+        <Routs />
+
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/program' component={Program} />
           <Route path='/chat' component={Chat} />
         </Switch>
+
       </div>
     );
   }
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>Someday I will be the chat!</div>
+    );
+  }
+}
+
+export default Chat;

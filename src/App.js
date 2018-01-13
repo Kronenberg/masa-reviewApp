f2b63c296a25980be709021a2a2b18a6b6dda84f
michael testerlabs <michael.k@testerlabs.com>
2018-01-13 23:20:24 +0200

    react SEO

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import { NavLink } from 'react-router-dom';
 import Routs from './routs';
 
+import {Helmet} from "react-helmet";
+
 class App extends Component {
   render() {
     return (
       <div>
+         <Helmet>
+                <meta charSet="utf-8" />
+                <title>MASA REVIEW APPLICATION</title>
+                <link rel="canonical" href="https://stark-atoll-57647.herokuapp.com/" />
+            </Helmet>
         <nav>
           <ul>
-            
             <li><NavLink to="/" activeClassName="selected">Home</NavLink></li>
             <li><NavLink to="/programs" activeClassName="selected">Programs</NavLink></li>
             <li><NavLink to="/chat" activeClassName="selected">Chat</NavLink></li>
-            
           </ul>
         </nav>
         <Routs />

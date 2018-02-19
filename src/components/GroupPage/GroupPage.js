df4e04e357e0e8e32522a4186bc0d3ab87f3b8d2
user.email <bigcheeseh@gmail.com>
2018-02-20 02:05:27 +0300

    routing bug

--- src/components/GroupPage/GroupPage.js
+++ src/components/GroupPage/GroupPage.js
@@ -4,6 +4,7 @@ import CreatePostModal from './components/CreatePostModal';
 import Post from './components/Post';
 import { fetchPosts } from '../../actions/events';
 import { withRouter } from 'react-router'
+import { NavLink } from 'react-router-dom';
 
 import './GroupPage.css';
 
@@ -18,32 +19,42 @@ class GroupPage extends Component {
         const { groupTitle } = this.props.match.params
         this.props.fetchPosts(groupTitle);
     }
+
     componentWillReceiveProps(nextProps){
         const { posts } = nextProps;
-
         this.setState({posts: [...posts]})
-    
+
+        if (nextProps.match.params !== this.props.match.params) {
+
+            const { groupTitle } = nextProps.match.params
+            this.props.fetchPosts(groupTitle);
+        }
     }
+
     shouldComponentUpdate(nextProps, nextState){
+
         if(nextState.posts !== this.state.posts){
             
             this.renderPosts(nextState.posts)
+            return false
         }
 
         if(nextState.postsElements !== this.state.postsElements){
             return true
         }
-        return true
+        return false
     }
 
     renderPosts(posts){
         const postsElements = posts.map((post, i) => {
             return (
-                <Post groupTitle={post.groupTitle}
-                      user={post.user}
-                      postId={post.postId}
-                      content={post.content}
-                      key={i} />
+                <div>
+                      <Post groupTitle={post.groupTitle}
+                            user={post.user}
+                            postId={post.postId}
+                            content={post.content}
+                            key={i} />
+                </div>
             )
         })
 
